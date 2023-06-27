95ec1f0ed0ba683b18e771c8fd365ac22769e55f
TornikeL38 <120584873+TornikeL38@users.noreply.github.com>
2023-06-27 13:06:36 +0400

    make background

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import SignIn from "./Components/SignIn";
+
+function App() {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/SignIn.jsx
@@ -0,0 +1,11 @@
+import "./style.css"
+
+
+export default function SignIn(){
+    return(
+        <div>
+            <div className="cornerDivLeft"></div>
+            <div className="cornerDivRight"></div>
+        </div>
+    )
+}
