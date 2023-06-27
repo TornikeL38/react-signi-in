3a39f953292bde0785b92e5598cf83d1d8a7bdee
TornikeL38 <120584873+TornikeL38@users.noreply.github.com>
2023-06-27 17:03:38 +0400

    finish markup

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -3,9 +3,17 @@ import "./style.css"
 
 export default function SignIn(){
     return(
-        <div>
+        <div className="screen">
             <div className="cornerDivLeft"></div>
             <div className="cornerDivRight"></div>
+            <div className="sigINContainer">
+                <h1>Create An Account</h1>
+                <p>Create an account to enjoy all the services without any ads for free!</p>
+                <input type="text" placeholder="Email adress"/>
+                <input type="password" placeholder="Password"/>
+                <button>Create Account</button>
+                <p id="alreadyHaveAccount">Already Have An Account? <span>Sign In</span></p>
+            </div>
         </div>
     )
 }
